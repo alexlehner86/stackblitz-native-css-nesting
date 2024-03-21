1772b76cac69469890b8cfe50f1bc444f6c6b45e
alexlehner86 <xander_lehner@gmx.at>
2024-03-21 09:52:50 +0100

    test global and encapsulated styles

---
+++ src/app/card1/card1.component.ts
@@ -0,0 +1,13 @@
+import { Component, ViewEncapsulation } from '@angular/core';
+
+@Component({
+  selector: 'app-card1',
+  standalone: true,
+  imports: [],
+  templateUrl: './card1.component.html',
+  styleUrl: './card1.component.css',
+  encapsulation: ViewEncapsulation.None,
+})
+export class Card1Component {
+
+}
--- src/main.ts
+++ src/main.ts
@@ -1,15 +1,21 @@
 import { Component } from '@angular/core';
 import { bootstrapApplication } from '@angular/platform-browser';
 import 'zone.js';
+import { Card1Component } from './app/card1/card1.component';
+import { Card2Component } from './app/card2/card2.component';
 
 @Component({
   selector: 'app-root',
   standalone: true,
+  imports: [Card1Component, Card2Component],
   template: `
     <h1>Native CSS Nesting Demo</h1>
     <a target="_blank" href="https://developer.mozilla.org/en-US/docs/Web/CSS/CSS_nesting/Using_CSS_nesting">
       Learn more about native CSS nesting
     </a>
+    <p>Doesn't work properly because Angular compiler converts the CSS nesting structure to unnested code :-(</p>
+    <app-card1></app-card1>
+    <app-card2></app-card2>
   `,
 })
 export class App {}
