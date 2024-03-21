d8ffe1d3350b7c13cbcd684329d0b5c78958843f
alexlehner86 <xander_lehner@gmx.at>
2024-03-21 09:15:20 +0100

    adapt homepage

--- src/main.ts
+++ src/main.ts
@@ -6,14 +6,12 @@ import 'zone.js';
   selector: 'app-root',
   standalone: true,
   template: `
-    <h1>Hello from {{ name }}!</h1>
-    <a target="_blank" href="https://angular.dev/overview">
-      Learn more about Angular
+    <h1>Native CSS Nesting Demo</h1>
+    <a target="_blank" href="https://developer.mozilla.org/en-US/docs/Web/CSS/CSS_nesting/Using_CSS_nesting">
+      Learn more about native CSS nesting
     </a>
   `,
 })
-export class App {
-  name = 'Angular';
-}
+export class App {}
 
 bootstrapApplication(App);
